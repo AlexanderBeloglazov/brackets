0cca52e659e9b43bd24dce1032c7043076c9fa89
AlexanderBeloglazov <105547135+AlexanderBeloglazov@users.noreply.github.com>
2022-07-24 13:11:43 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let checkArr = []
+
+  let brackets = Object.fromEntries(bracketsConfig)
+
+  for (let i = 0; i < str.length; i++) {
+    
+    checkArr.length === 0 ? checkArr.push(str[i]) : (str[i] === brackets[checkArr[checkArr.length - 1]] ? checkArr.pop() : checkArr.push(str[i])) 
+   
+    
+  }
+
+  if(checkArr.length === 0) {
+    return true
+  }
+  else {
+    return false
+  }
+
 }
